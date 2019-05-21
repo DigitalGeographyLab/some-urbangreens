8c5bb52a5c97fd665c717e613409ff744d78fffd
Vuokko Heikinheimo <vuokko.heikinheimo@gmail.com>
2019-05-21 17:52:16 +0300

    script for hotspot comparison using jaccard index

---
+++ calculate_jaccard.py
@@ -0,0 +1,113 @@
+"""
+Script for calculating the Jaccard index between the top 10 % spatial
+hotspots of user-generated data sets in urban green areas.
+
+Jaccard index: intersection divided by the union of two sets
+
+Steps:
+- Read in dataframe containing data counts in a grid
+- Get top 10 % of each column (the hotspots)
+- Calculate jaccard index (intersection over union) for each column pair
+- save output matrix as csv
+
+Input:
+    YKR grid data with grid cells as rows
+    and counts of user-generated content as columns (can be a shapefile, csv, whatever pandas reads)
+Output:
+    csv file with crosstabulated jaccard scores for data set pairs
+
+Vuokko H. 21 May 2019
+
+"""
+
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from sklearn.metrics import jaccard_score
+
+#-----------------------------
+# Read input data
+#------------------------------
+
+# input data
+fp = r"D:/ViherSOME/Data/YKR_grid/YKR_250m_mobilebigdata.shp"
+data = gpd.read_file(fp)
+
+#check input data
+data.head()
+data.columns.values
+
+# Replace zero with nan to exclude from classification
+data.replace(to_replace=0, value=np.nan, inplace=True)
+
+#select only needed columns (NOW THESTING WITH A LIMITED SET..)
+data_values = data[['insta_user','insta_gree','flickr_use','ZROP H17']]
+
+#------------------------------
+# Get top 10 % of each column
+#------------------------------
+
+# Discretize column values (axis = 0) into 10 equal-sized buckets; output as bins
+#https://pandas.pydata.org/pandas-docs/version/0.22/generated/pandas.qcut.html
+#data_q = data_values.apply(lambda x: pd.qcut(x, q=10, duplicates="drop"), axis = 0)
+
+# Discretize column values (axis = 0) into 10 equal-sized buckets; output as flexible-length labels
+data_q = data_values.apply(lambda x: pd.qcut(x,
+                                             q=10,
+                                             duplicates="drop",
+                                             labels = range(0, len(pd.qcut(x,q=10, duplicates="drop").value_counts()))),
+                                             axis = 0)
+
+
+# Get all grid squares which belong to top quintile of each column
+top10 = data_q.apply(lambda x: x[x==x.max()], axis = 0)
+
+# Join with original grid IDs and re-set index
+top10 = pd.merge(pd.DataFrame(data["ID"]), top10, left_index=True, right_index = True, how = 'left')
+top10.set_index("ID", drop=True, inplace =True)
+
+#conver to binary 1 0
+top10 = top10.notnull().astype('int')
+
+#------------------------------
+# CALCULATE JACCARD
+#-------------------------------
+
+# list column values (NOTE! these should only contain columns with actual data, not eg. id values..)
+data_names = list(top10.columns.values)
+
+#Create output matrix for the results
+jaccard_results = pd.DataFrame(index=data_names, columns = data_names)
+
+# For each pair of data, calculate intersection, union and intersection over union...
+for name1 in data_names:
+    for name2 in data_names:
+
+        intersection = len(top10[(top10[name1]==1) & (top10[name2]==1)])
+        union =  intersection + len(top10[(top10[name1]==0) & (top10[name2]==1)]) + len(top10[(top10[name1]==1) & (top10[name2]==0)])
+        jaccard = intersection / union
+
+        print("Pairwise comparison of", name1, name2)
+        print("Intersection:", intersection)
+        print("Union:", union)
+        print("Jaccard:", jaccard, "\n")
+
+        jaccard_results.at[name1, name2] = jaccard
+
+
+jaccard_results.to_csv(r"jaccard_results.csv")
+
+
+"""
+# Creating quintiles one-by one: 
+pd.qcut(data['ZROP H17'],q=10, duplicates="drop").value_counts()
+pd.qcut(data['insta_user'],q=10, duplicates="drop").value_counts()
+pd.qcut(data['insta_gree'],q=10, duplicates="drop").value_counts()
+pd.qcut(data['flickr_use'],q=10, duplicates="drop").value_counts()
+pd.qcut(data['ZROP H17'],q=10, duplicates="drop").value_counts()
+
+pd.qcut(data['insta_user'],q=10, duplicates="drop", labels = range(0, len(pd.qcut(data['insta_user'],q=10, duplicates="drop").value_counts())))
+pd.qcut(data['insta_gree'],q=10, duplicates="drop")
+pd.qcut(data['flickr_use'],q=10, duplicates="drop")
+pd.qcut(data['ZROP H17'],q=10, duplicates="drop")
+"""
