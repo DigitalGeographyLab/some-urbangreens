4bb05de28d38d9473ebf58d959181b2cb7850f04
VuokkoH <vuokko.heikinheimo@helsinki.fi>
2019-07-09 16:52:13 +0300

    udpdated and renamed jaccard files

--- data_to_greenfingers.py
+++ data_to_greenfingers.py
@@ -1,46 +1,100 @@
 
 """
-Join data to green area grids.
+Join user-generated data sets to grid (count of users, count of userdays for social media data)
 
-# Input
-    - grid containing cells with their centroids inside green fingers (VISTRA). Layer created in QGIS
-    - grid containing count data from social media and PPGIS
-    - grid containing relative hourly population density based on mobile phone data (Bergroth 2019)
-
-# Output
-    - grid with information from user-generated data sources covering the green fingers in Helsinki
-    - stored as a layer in geopackage
-
-Author: Vuokko H. 4.7.2019
-
+# INPUT
+    - GRID
+    - POINTS
 """
+
 import geopandas as gpd
 import pandas as pd
 import os
 
-# Grid cells with centroids in green areas (generated using QGIS :) )
-fp = r"D:/ViherSOME/Data/YKR_grid/YKR_250m_Helsinki_centroidin_VISTRA_Vihersormi_fixedgeom.shp"
-greengrid = gpd.read_file(fp)
 
-# Full grid with social media data and PPGIS counts
-datadir = r"P:\h510\some\data\finland\vihersome_temp"
-grid = gpd.read_file(os.path.join(datadir, r'urbangreens_grid.gpkg'), layer="grid")
+def create_user_days(points):
+    """ Generates a combination of day + month + user as a series"""
 
-# Mobile phone data for the whole Helsinki Region
-fp = "D:\ViherSOME\Data\Mobile_phone\dynamic_population_24H.csv"
-mobile = pd.read_csv(fp)
+    # CONCATENATE USER+ DAY + MONTH
+    points["time_local"] = pd.to_datetime(points["time_local"])
+    points_with_userday = points.apply(lambda x: str(x["time_local"].year) + str(x["time_local"].day) + str(x["time_local"].month) + "_" + str(x["userid"]), axis=1)
 
-# Drop columns to avoid duplicate columns in merge
-grid.drop(columns= ["x", 'GRIDCODE', 'x', 'y', 'xyind', 'Shape_Leng', 'Shape_Area', "geometry"], inplace = True)
-greengrid.drop(columns= ['join_fid', 'join_id', 'join_kohde', 'distance'], inplace = True)
-mobile.drop(columns= ["Unnamed: 0", "geometry"], inplace = True)
+    return points_with_userday
 
-#merge social media, PPGIS to greengrid
-greengrid = greengrid.merge( grid, how = "inner",on = "ID", right_index = False)
 
-# merge mobile phone data to grid:
-greengrid = greengrid.merge(mobile, left_on = "ID", right_on = "YKR_ID")
+def join_points_to_grid(points, grid):
+    "joins attributes from points to intersecting polygons (left join)"
 
-# save output to file
-print("Saving output to geopackage")
-greengrid.to_file(os.path.join(datadir, "urbangreens_grid.gpkg"), layer="greengrid", driver="GPKG")
+    # re-project points to grid crs
+    points = points.to_crs(grid.crs)
+    print("projected points into:", str(points.crs))
+    grid_join = gpd.tools.sjoin(grid, points,  how="left", op="intersects")
+
+    return grid_join
+
+
+def count_per_grid(grid_join, grid_orig, value_column = "userday", group_column = "GRIDCODE"):
+    """ groups the join (between grid and points), and counts unique values of selected column for each grid cell.
+    Adds a count column to the original dataframe."""
+
+    counts = pd.DataFrame(grid_join.groupby(group_column)[value_column].nunique())
+    grid_ok = pd.merge(grid_orig, counts, left_on= group_column, right_index=True)
+
+    return grid_ok
+
+if __name__ == "__main__":
+
+    # Input layers:
+
+    # YKR grid polygons:
+    grid_fp = r"D:\ViherSOME\Data\YKR_grid\YKR_250m_Helsinki.shp"
+    grid = gpd.read_file(grid_fp)
+    polygon_id = "ID"
+
+    datadir = r"P:\h510\some\data\finland\vihersome_temp"
+
+    # List layers for analysis (these exist in the geodatabase
+    # Dictionary contains info of layername: type
+    layers = {"PPGIS2050_greens" : "PPGIS",
+              "PPGISpark_greens" : "PPGIS",
+              "insta_helsinki_2015_greens": "socialmedia",
+              "flick_helsinki_2015_greens": "socialmedia",
+              "twitt_helsinki_2017_greens": "socialmedia"}
+
+    # Dictionary contains info of layername: unique user id column
+    usercolumns = {"PPGIS2050_greens" :'user_id',
+              "PPGISpark_greens":'respondent',
+              "insta_helsinki_2015_greens":'userid',
+              "flick_helsinki_2015_greens":'userid',
+              "twitt_helsinki_2017_greens":'userid'}
+
+    for layer, type in layers.items():
+
+        # Read in layer from geopackage
+        df = gpd.read_file(os.path.join(datadir, r'urbangreens.gpkg'), layer=layer)
+        df.crs = {'init': 'epsg:4326'}
+
+        if type == "socialmedia":
+            # continue only with user id column and timestamp
+            df = df[[usercolumns.get(layer), "time_local", "geometry"]]
+
+            # apply:
+            df["userday"] = create_user_days(df)
+
+        if type == "PPGIS":
+            # continue only with user id column and timestamp
+            df = df[[usercolumns.get(layer), "geometry"]]
+
+        # Join point info to intersecting grid; left join which returns all combinations
+        join = join_points_to_grid(df, grid)
+        grid = count_per_grid(join, grid, value_column = usercolumns.get(layer), group_column = polygon_id)
+
+        grid.rename(columns = {usercolumns.get(layer): layer[:9] + "_users"}, inplace=True)
+
+        if type == "socialmedia":
+            #Count also userdays per grid
+            grid = count_per_grid(join, grid, value_column="userday", group_column=polygon_id)
+            grid.rename(columns={"userday": layer[:9] + "_userdays"}, inplace=True)
+
+    #save output to file
+    grid.to_file(os.path.join(datadir, "urbangreens_grid.gpkg"), layer = "grid", driver="GPKG")
---
+++ join_grid_to_greenfingers.py
@@ -0,0 +1,55 @@
+
+"""
+Join data to green area grids.
+
+# Input
+    - grid containing cells with their centroids inside green fingers (VISTRA). Layer created in QGIS
+    - grid containing count data from social media and PPGIS
+    - grid containing relative hourly population density based on mobile phone data (Bergroth 2019)
+
+# Output
+    - grid with information from user-generated data sources covering the green fingers in Helsinki
+    - stored as a layer in geopackage
+
+Author: Vuokko H. 4.7.2019
+
+"""
+import geopandas as gpd
+import pandas as pd
+import os
+
+# Grid cells with centroids in green areas (generated using QGIS :) )
+fp = r"D:/ViherSOME/Data/YKR_grid/YKR_250m_Helsinki_centroidin_VISTRA_Vihersormi_fixedgeom.shp"
+greengrid = gpd.read_file(fp)
+
+# Full grid with social media data and PPGIS counts
+datadir = r"P:\h510\some\data\finland\vihersome_temp"
+grid = gpd.read_file(os.path.join(datadir, r'urbangreens_grid.gpkg'), layer="grid")
+
+# greengrid with strava max athlete per grid
+datadir = r"P:\h510\some\data\finland\vihersome_temp"
+strava = gpd.read_file(os.path.join(datadir, r'urbangreens_grid.gpkg'), layer="strava_athl_max")
+
+# Mobile phone data for the whole Helsinki Region
+fp = "D:\ViherSOME\Data\Mobile_phone\dynamic_population_24H.csv"
+mobile = pd.read_csv(fp)
+
+# Drop columns to avoid duplicate columns in merge
+grid.drop(columns= ["x", 'GRIDCODE', 'x', 'y', 'xyind', 'Shape_Leng', 'Shape_Area', "geometry"], inplace = True)
+greengrid.drop(columns= ['join_fid', 'join_id', 'join_kohde', 'distance'], inplace = True)
+mobile.drop(columns= ["Unnamed: 0", "geometry"], inplace = True)
+strava.drop(columns = ['GRIDCODE', 'x', 'y', 'xyind', 'Shape_Leng', 'Shape_Area',
+       'join_fid', 'join_id', 'join_kohde', 'distance', 'geometry'], inplace = True)
+
+#merge social media, PPGIS to greengrid
+greengrid = greengrid.merge( grid, how = "inner",on = "ID", right_index = False)
+
+# merge mobile phone data to grid:
+greengrid = greengrid.merge(mobile, left_on = "ID", right_on = "YKR_ID")
+
+# merge strava data to grid
+greengrid = greengrid.merge(strava, on = "ID")
+
+# save output to file
+print("Saving output to geopackage")
+greengrid.to_file(os.path.join(datadir, "urbangreens_grid.gpkg"), layer="greengrid", driver="GPKG")
